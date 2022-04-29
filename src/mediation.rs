d63c3458afb4bc0175db77ab72e22b81e95a7033
Christian M <chriamue@gmail.com>
2022-04-29 19:49:25 +0200

    :construction: added tests for connect

--- src/mediation.rs
+++ src/mediation.rs
@@ -64,33 +64,118 @@ impl Default for ConnectionRequest {
     }
 }
 
+pub async fn create_invitation(label: String) -> Value {
+    let request = CreateInvitation { label: label };
+    let client = reqwest::Client::new();
+    let res = client
+        .post("http://localhost:8080/outofband/create-invitation")
+        .json(&request)
+        .send()
+        .await
+        .unwrap();
+    res.json().await.unwrap()
+}
+
+pub async fn accept_invitation(label: String, invitation: Value) -> String {
+    let mut invitation = invitation.clone();
+    invitation["my_label"] = label.into();
+    let client = reqwest::Client::new();
+    let res = client
+        .post("http://localhost:8080/outofband/accept-invitation")
+        .json(&invitation)
+        .send()
+        .await
+        .unwrap();
+    let accept_response: Value = res.json().await.unwrap();
+    let connection_id = accept_response["connection_id"].as_str().unwrap();
+    connection_id.to_string()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use didcomm_rs::Error;
+    use std::{thread, time};
 
+    #[ignore = "not now"]
     #[tokio::test]
     async fn send_create_invitation() -> Result<(), Error> {
-        let request = CreateInvitation{
-            label: "did-planning-poker".to_string()
-        };
+        let invitation: Value = create_invitation("did-planning-poker".to_string()).await;
+        let connection_id = accept_invitation("user".to_string(), invitation).await;
+
         let client = reqwest::Client::new();
         let res = client
-            .post("http://localhost:8080/outofband/create-invitation")
-            .json(&request)
-            .send()
-            .await
-            .unwrap();
-        let mut invitation: Value = res.json().await.unwrap();
-        invitation["my_label"] = "user".to_string().into();
-
-        let res = client
-            .post("http://localhost:8080/outofband/accept-invitation")
-            .json(&invitation)
+            .post(format!(
+                "http://localhost:8080/connections/{}/accept-request",
+                connection_id
+            ))
             .send()
             .await
             .unwrap();
         println!("{}", res.text().await.unwrap());
+
+        Ok(())
+    }
+
+    #[ignore = "not now"]
+    #[tokio::test]
+    async fn send_mediation_request() -> Result<(), Error> {
+        let invitation: Value = create_invitation("did-planning-poker".to_string()).await;
+        let connection_id = accept_invitation("user".to_string(), invitation).await;
+        let mut request = MediationRequest::default();
+        request.id = connection_id;
+        println!("{}", serde_json::to_string(&request).unwrap());
+
+        let client = reqwest::Client::new();
+        let res = client
+            .post("http://localhost:8081/")
+            .json(&request)
+            .send()
+            .await
+            .unwrap();
+        println!("{}", res.text().await.unwrap());
+
+        Ok(())
+    }
+
+    //#[ignore = "not now"]
+    #[tokio::test]
+    async fn send_connection_request() -> Result<(), Error> {
+        let mut invitation: Value = create_invitation("did-planning-poker".to_string()).await;
+        invitation["my_label"] = "user".to_string().into();
+        let connection_id = accept_invitation("user".to_string(), invitation.clone()).await;
+
+        let mut request = ConnectionRequest::default();
+
+        let mut request = MediationRequest::default();
+        request.id = connection_id;
+
+        let (mut socket, response) =
+            connect(Url::parse("ws://localhost:8082/").unwrap()).expect("Can't connect");
+
+        println!("Connected to the server");
+        println!("Response HTTP code: {}", response.status());
+        println!("Response contains the following headers:");
+        for (ref header, _value) in response.headers() {
+            println!("* {}", header);
+        }
+        let json = serde_json::to_string(&request.clone()).unwrap();
+        //println!("{}", json);
+
+        socket
+            .write_message(Message::Text(
+                json,
+            ))
+            .unwrap();
+
+        let _handler = thread::spawn(move || {
+            if socket.can_read() {
+                let msg = socket.read_message().expect("Error reading message");
+                println!("Received: {}", msg);
+            }
+        });
+
+        thread::sleep(time::Duration::from_millis(2000));
 
         Ok(())
     }
