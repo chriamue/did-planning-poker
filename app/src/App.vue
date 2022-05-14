b1113c278b575e0634177b6507f9a3242a737d51
Christian M <chriamue@gmail.com>
2022-05-14 20:02:28 +0200

    :tada: added app

---
+++ app/src/App.vue
@@ -0,0 +1,34 @@
+<script setup>
+import InvitationComp from "./components/InvitationComp.vue";
+</script>
+
+<template>
+  <header>
+    <h1>Did Planning Poker</h1>
+  </header>
+
+  <main>
+    <Suspense>
+      <invitation-comp />
+
+      <!-- loading state via #fallback slot -->
+      <template #fallback> Loading... </template>
+    </Suspense>
+  </main>
+</template>
+
+<style>
+@import "./assets/base.css";
+
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+
+  font-weight: normal;
+}
+
+header {
+  line-height: 1.5;
+}
+</style>
---
+++ app/src/components/InvitationComp.vue
@@ -0,0 +1,13 @@
+<script setup>
+const invitation = await fetch(`http://127.0.0.1:8000/invitation`).then(async (r) => JSON.stringify((await r.json()).invitation))
+</script>
+
+<template>
+  <div class="invitation">
+    <textarea v-model="invitation" cols="100" rows="6">
+    </textarea>
+  </div>
+</template>
+
+<style scoped>
+</style>
