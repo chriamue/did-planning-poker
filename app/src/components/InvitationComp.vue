d1fab3d1e6fc4a073619d0c58f97f913376aa769
Christian M <chriamue@gmail.com>
2022-05-15 16:01:52 +0200

    :sparkles: added wasm pack plugin

--- app/src/components/InvitationComp.vue
+++ app/src/components/InvitationComp.vue
@@ -1,11 +1,21 @@
 <script setup>
-const invitation = await fetch(`http://127.0.0.1:8000/invitation`).then(async (r) => JSON.stringify((await r.json()).invitation))
+import PingComp from './PingComp.vue';
+const invitation = await fetch(`http://127.0.0.1:8000/invitation`).then(async (r) => (await r.json()).invitation)
+const did = invitation.services[0].recipientKeys[0];
+const invitation_str = JSON.stringify(invitation);
 </script>
 
 <template>
   <div class="invitation">
-    <textarea v-model="invitation" cols="100" rows="6">
+    <h3>{{did}}</h3>
+    <textarea v-model="invitation_str" cols="100" rows="6">
     </textarea>
+        <Suspense>
+      <ping-comp v-if="invitation" host="http://127.0.0.1:8000/didcomm" :did="did" />
+
+      <!-- loading state via #fallback slot -->
+      <template #fallback> Loading... </template>
+    </Suspense>
   </div>
 </template>
 
--- app/vite.config.js
+++ app/vite.config.js
@@ -2,10 +2,11 @@ import { fileURLToPath, URL } from 'url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import wasmPack from 'vite-plugin-wasm-pack';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), wasmPack('../')],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
