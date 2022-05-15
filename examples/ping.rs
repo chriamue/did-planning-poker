921c254f866e78f2094f8276026a18f89d82f1ae
Christian M <chriamue@gmail.com>
2022-05-15 09:09:07 +0200

    :sparkles: added ping example

--- examples/ping.rs
+++ examples/ping.rs
@@ -1,12 +1,23 @@
-use std::env;
-use did_key::{generate, DIDCore, Ed25519KeyPair};
+use did_key::{generate, X25519KeyPair};
+use did_planning_poker::mediation::create_invitation;
 use did_planning_poker::ping::ping;
+use serde_json::Value;
 
 #[tokio::main]
 async fn main() {
-    let key = generate::<Ed25519KeyPair>(None);
-    ping(&key).await;
+    let key = generate::<X25519KeyPair>(None);
+    let host = "http://localhost:8000/didcomm".to_string();
 
-    let name = env::args().skip(1).next();
-    println!("Hello, {}!", name.unwrap_or("world".into()));
+    let invitation: Value = create_invitation("did-planning-poker".to_string()).await;
+
+    let inv_did = invitation["invitation"]["services"][0]["recipientKeys"]
+        .as_array()
+        .unwrap()[0]
+        .as_str()
+        .unwrap()
+        .to_string();
+    println!("{}", invitation);
+    println!("PING {} @ {}", inv_did, host);
+    let duration_millis = ping(&key, inv_did, host).await.unwrap();
+    println!("time = {}ms", duration_millis);
 }
--- src/ping.rs
+++ src/ping.rs
@@ -1,31 +1,31 @@
 use did_key::*;
-use did_key::{generate, DIDCore, Ed25519KeyPair};
-use didcomm_rs::{
-    crypto::{CryptoAlgorithm, SignatureAlgorithm, Signer},
-    Error, JwmHeader, Message, MessageType,
-};
-use rand_core::OsRng;
-use serde::{Deserialize, Serialize};
-use serde_json::Value;
-use url::Url;
-use uuid::Uuid;
+use didcomm_mediator::message::sign_and_encrypt;
 use didcomm_mediator::protocols::trustping::TrustPingResponseBuilder;
+use std::time::Instant;
 
-use reqwest::header::{ACCEPT, CONTENT_TYPE};
+pub async fn ping(key: &KeyPair, did_to: String, host: String) -> Result<u32, &'static str> {
+    let did_doc = key.get_did_document(Default::default());
+    let did_from = did_doc.id.to_string();
 
-pub async fn ping(key: &KeyPair) {
-    let did = key.get_did_document(Default::default()).id;
-    let request = TrustPingResponseBuilder::new().did(did).build().unwrap();
+    let request = TrustPingResponseBuilder::new()
+        .build()
+        .unwrap()
+        .from(&did_from);
+
+    let request = sign_and_encrypt(&request, &did_to, &key);
+
+    let start = Instant::now();
 
     let client = reqwest::Client::new();
-    let res = client
-        .post("http://localhost:8000/didcomm")
-        .header(CONTENT_TYPE, "application/json")
-        //.body(jws_string)
-        .send()
-        .await
-        .unwrap();
-    println!("{}", res.text().await.unwrap());
+    let res = client.post(host).json(&request).send().await.unwrap();
+
+    if !res.status().is_success() {
+        return Err("ping failed");
+    }
+
+    let duration = start.elapsed();
+
+    Ok(duration.as_millis() as u32)
 }
 
 #[cfg(test)]
@@ -33,39 +33,27 @@ mod tests {
     use super::*;
     use crate::mediation::create_invitation;
     use didcomm_rs::Error;
-    use std::{thread, time};
+    use serde_json::Value;
 
     //#[ignore = "not now"]
     #[tokio::test]
     async fn send_ping() -> Result<(), Error> {
-        let sign_keypair = ed25519_dalek::Keypair::generate(&mut OsRng);
-        let key = generate::<Ed25519KeyPair>(None);
-        let did_doc = key.get_did_document(Config::default());
-        let did = did_doc.id.clone();
+        let key = generate::<X25519KeyPair>(None);
+        let host = "http://localhost:8000/didcomm".to_string();
 
-        let mut invitation: Value = create_invitation("did-planning-poker".to_string()).await;
+        let invitation: Value = create_invitation("did-planning-poker".to_string()).await;
+
+        let inv_did = invitation["invitation"]["services"][0]["recipientKeys"]
+            .as_array()
+            .unwrap()[0]
+            .as_str()
+            .unwrap()
+            .to_string();
         println!("{}", invitation);
 
-        let jws_string = Message::new()
-            .m_type("https://didcomm.org/trust-ping/2.0/ping")
-            .from(&did)
-            .body(r#"{"response_requested": true}"#)
-            .as_flat_jws(&SignatureAlgorithm::EdDsa)
-            .sign(SignatureAlgorithm::EdDsa.signer(), &sign_keypair.to_bytes())?;
-        let jws_object: Value = serde_json::from_str(&jws_string)?;
-        assert!(jws_object["protected"].as_str().is_some());
-        println!("{}", jws_object);
-
-        let client = reqwest::Client::new();
-        let res = client
-            .post("http://localhost:8081/")
-            .header(CONTENT_TYPE, "application/ssi-agent-wire")
-            .body(jws_string)
-            .send()
-            .await
-            .unwrap();
-        println!("{}", res.text().await.unwrap());
-
+        let duration_millis = ping(&key, inv_did, host).await.unwrap();
+        assert!(duration_millis > 1);
+        assert!(duration_millis < 1000);
         Ok(())
     }
 }
